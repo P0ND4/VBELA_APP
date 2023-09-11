5dac17b8070a3b9708096bc84f6166b84f879b71
Melvin Colmenares <melvincolmenares.m@gmail.com>
2023-09-11 14:53:54 -0400

    Error environment variables fixed

--- app.config.js
+++ app.config.js
@@ -1,7 +1,8 @@
 export default {
   "expo": {
     "android": {
-      "googleServicesFile": process.env.GOOGLE_SERVICES || process.env.EXPO_PUBLIC_GOOGLE_SERVICES
+      "googleServicesFile": process.env.GOOGLE_SERVICES || process.env.EXPO_PUBLIC_GOOGLE_SERVICES,
+      "package": "com.app.vbela",
     },
     "extra": { "eas": { "projectId": "0dd838a6-95db-4883-9a7f-7e6112496cd0" } },
     "owner": "lmacml"
