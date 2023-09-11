569c5b46e148e8a1a190bcb7cab46ba1c2112512
Melvin Colmenares <melvincolmenares.m@gmail.com>
2023-09-11 16:53:22 -0400

    Bugs and errors fixed

--- app.config.js
+++ app.config.js
@@ -3,6 +3,7 @@ export default {
     "android": {
       "googleServicesFile": process.env.GOOGLE_SERVICES || process.env.EXPO_PUBLIC_GOOGLE_SERVICES,
       "package": "com.app.vbela",
+      "versionCode": 40,
     },
     "extra": { "eas": { "projectId": "0dd838a6-95db-4883-9a7f-7e6112496cd0" } },
     "owner": "lmacml"
