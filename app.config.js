a1ea7ddca1307c0ba5c2beb72076d28cd526c8d4
Melvin Colmenares <melvincolmenares.m@gmail.com>
2023-12-03 00:06:39 -0400

    i forgot change the versionCode

--- app.config.js
+++ app.config.js
@@ -50,7 +50,7 @@ export default {
     android: {
       googleServicesFile: process.env.GOOGLE_SERVICES || process.env.EXPO_PUBLIC_GOOGLE_SERVICES,
       package: "com.app.vbela",
-      versionCode: 48,
+      versionCode: 49,
     },
     extra: { eas: { projectId: "0dd838a6-95db-4883-9a7f-7e6112496cd0" } },
     owner: "lmacml",
