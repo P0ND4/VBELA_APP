27d3500d26f9073b9da7f969b49fe3c2ffae6cdf
Melvin Colmenares <melvincolmenares.m@gmail.com>
2024-03-29 00:59:28 -0400

    i forgot to send the version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "VBELA",
     slug: "vbela",
-    version: "3.7.0",
+    version: "3.8.0",
     orientation: "portrait",
     icon: "./assets/logo.png",
     userInterfaceStyle: "light",
@@ -25,7 +25,7 @@ export default {
     android: {
       googleServicesFile: process.env.EXPO_PUBLIC_GOOGLE_SERVICES,
       package: "com.app.vbela",
-      versionCode: 56,
+      versionCode: 57,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#FFFFFF",
